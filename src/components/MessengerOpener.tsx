588d70e4a20dec9c03ac231314f85ca2bcaf353a
Anatoliy Z <zolotukhinwebpr@gmail.com>
2021-01-15 01:39:16 +0700

    com

--- src/components/MessengerOpener.tsx
+++ src/components/MessengerOpener.tsx
@@ -1,5 +1,5 @@
-import { Drawer, DrawerProps } from "@material-ui/core";
 import React from "react";
+import { Drawer, DrawerProps } from "@material-ui/core";
 
 interface MessengerOpener extends DrawerProps {}
 
@@ -7,18 +7,7 @@ export const MessengerOpener: React.FC<MessengerOpener> = (props) => {
   const { children, ...restProps } = props;
 
   return (
-    <Drawer
-      {...restProps}
-      ModalProps={{
-        BackdropProps: {
-          style: {
-            backgroundColor: "rgba(0,0,0,0.20)",
-          },
-        },
-      }}
-      style={{ zIndex: 1400 }}
-      anchor="right"
-    >
+    <Drawer {...restProps} style={{ zIndex: 1400 }} anchor="right">
       {children}
     </Drawer>
   );
