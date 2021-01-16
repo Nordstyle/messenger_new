81fcc8b8314d73e3ee32d8d5fae33d5d4e1a56c7
Anatoliy Z <zolotukhinwebpr@gmail.com>
2021-01-16 19:26:58 +0700

    test

--- src/components/MessengerMessages/MessengerVotingMessage/MessengerVotingMessage.tsx
+++ src/components/MessengerMessages/MessengerVotingMessage/MessengerVotingMessage.tsx
@@ -1,5 +1,5 @@
-import { Grid } from "@material-ui/core";
 import React from "react";
+import { Grid } from "@material-ui/core";
 import { VotingClient } from "../../../types";
 import { MessengerVotingCard } from "./MessengerVotingCard";
 import { MessengerVotingResultLine } from "./MessengerVotingResultLine/MessengerVotingResultLine";
